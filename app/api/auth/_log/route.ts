7e58171cff79f4bb4ea9235a4bc37cd77214ffc7
Aaron Lee <aaron.lee@gusto.com>
2025-02-25 16:01:06 -0500

    anotha one

--- app/api/auth/_log/route.ts
+++ app/api/auth/_log/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 
-export async function POST(req: Request) {
+export async function POST() {
   // Accept and log the request but don't do anything with it
   return NextResponse.json({ success: true })
 } 
